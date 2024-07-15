e27844d66b7659c8cb0fa62d0234ef2a65ae9553
EtienneKuenzel <etienne_kuenzel@web.de>
2024-07-15 11:26:29 +0200

    Update Parser

--- MARL/flatland-starter-kit-master/baselines/multi_agent_training.py
+++ MARL/flatland-starter-kit-master/baselines/multi_agent_training.py
@@ -585,8 +585,8 @@ if __name__ == "__main__":
     parser.add_argument("--num_threads", help="number of threads PyTorch can use", default=1, type=int)
 
     parser.add_argument("--curriculum", help="choose a curriculum(replace ___ with PMD in any sequence)P=Pathfinding, M=Malfunction, D=Deadlock: custom___", default="customPMD", type=str)
-    parser.add_argument("--envchange", help="time after environment change", default=800, type=int)
-    parser.add_argument("--expansion", help="time after expansion", default=3200, type=int)
+    parser.add_argument("--envchange", help="time after environment change", default=80000, type=int)
+    parser.add_argument("--expansion", help="time after expansion", default=320000, type=int)
     parser.add_argument("--policy", help="choose policy: CDE,DQN, EWC, PAU", default="CDE", type=str)
     parser.add_argument("--runs", help="repetitions of the training loop", default=1, type=int)
     training_params = parser.parse_args()
