3ec93acdf5db49c8e8d83309fcc82637cd5d3422
EtienneKuenzel <etienne_kuenzel@web.de>
2025-02-06 19:05:46 +0100

    Update dddqn_policy.py

--- MARL/flatland-starter-kit-master/baselines/reinforcement_learning/dddqn_policy.py
+++ MARL/flatland-starter-kit-master/baselines/reinforcement_learning/dddqn_policy.py
@@ -19,7 +19,7 @@ from reinforcement_learning.gnt import GnT
 import torch.nn.functional as F
 
 
-class Continual_DQN_Expansion():
+class Continual_DQN_Expansionold():
     """Continual DDQN Expansion(CDE)"""
     def __init__(self, state_size, action_size, parameters, evaluation_mode=False):
         self.networks = [[]]
@@ -76,13 +76,81 @@ class Continual_DQN_Expansion():
                                                 self.networks[-1][network].optimizer, self.networks[-1][network].ewc_loss, self.networks[-1][network].memory,
                                                 self.networks[-1][network].loss, self.networks[-1][network].params,
                                                 self.networks[-1][network].p_old)
-            #self.networks[-1][network].update_ewc()
-            self.networks[-1][network].update_mas()
+            self.networks[-1][network].update_ewc()
             self.networks[-1][network].freeze = True
 
     def set_evaluation_mode(self, evaluation_mode):
         self.evaluation_mode = evaluation_mode
+    def get_name(self):
+        return "CDE" + str(self.act_rotation)
+    def get_activation(self):
+        result = []
+        for param_tuple in self.networks[-1][self.act_rotation].get_weigths():
+            a, b = param_tuple
+            result.append([a.tolist(), b.tolist()])
+        return result
+    def get_net(self):
+        return self.act_rotation
+    def get_fisher(self):
+        return self.get_fisher_diag(self.qnetwork_local, self.memory, self.params)
+class Continual_DQN_Expansion():
+    """Continual DDQN Expansion(CDE)"""
+    def __init__(self, state_size, action_size, parameters, evaluation_mode=False):
+        self.networks = [[]]
+        self.state_size = state_size
+        self.action_size = action_size
+        self.parameters = parameters
+        self.evaluation_mode = evaluation_mode
+        self.networks[0].append(subCDE_Policy(state_size, action_size, parameters, evaluation_mode))
+        self.anchor_number = self.parameters.anchors
+        self.act_rotation = 0
+        self.evaluation_mode = False
+        self.networkEP = []
+        self.networkEP_scores = []
+        self.networkEP_completions = []
 
+    import numpy as np
+
+    def act(self, handle, state, eps=0., eval=False):
+        actions = [network.act(handle, state, eps) for network in self.networks[-1]]
+        print(actions)
+        counts = np.bincount(actions)
+        print(counts)
+        top_actions = np.where(counts == counts.max())[0]
+        print(top_actions)
+        print("------------")
+        return np.random.choice(top_actions)
+
+    def network_rotation(self, score, completions):
+        pass
+    def step(self, handle, state, action, reward, next_state, done):
+        for network in self.networks[-1]:
+            network.step(handle, state, action, reward, next_state, done)
+    def expansion(self):
+        last_networks = self.networks[-1]
+        self.act_rotation = 0
+        # Use nlargest to keep the top 2 elements with the highest average score, including their indexes.
+        top_networks = heapq.nlargest(self.anchor_number, [(numpy.average(x.score), i, x) for i, x in enumerate(last_networks)], key=lambda item: item[0])
+        # Extract the elements (discard the average scores) and indexes, then update self.networks[-1].
+        top_network_indexes = [i for _, i, _ in top_networks]
+        self.networks[-1] = [x for _, _, x in top_networks]
+        self.networkEP.append(top_network_indexes)
+        self.networkEP_scores.append([numpy.mean(last_networks[x].score) for x in top_network_indexes])
+        self.networkEP_completions.append([numpy.mean(last_networks[x].completions) for x in top_network_indexes])
+        #double the amount of networks first half= EWC last half =PAU
+        a = len(self.networks[-1])
+        for network in range(a):
+            self.networks[-1].append(subCDE_Policy(self.state_size, self.action_size, self.parameters, self.evaluation_mode, freeze=False, initialweights=self.networks[-1][network].get_weigths()))
+            self.networks[-1][-1].set_parameters(self.networks[-1][network].qnetwork_local,
+                                                self.networks[-1][network].qnetwork_target,
+                                                self.networks[-1][network].optimizer, self.networks[-1][network].ewc_loss, self.networks[-1][network].memory,
+                                                self.networks[-1][network].loss, self.networks[-1][network].params,
+                                                self.networks[-1][network].p_old)
+            self.networks[-1][network].update_ewc()
+            self.networks[-1][network].freeze = True
+
+    def set_evaluation_mode(self, evaluation_mode):
+        self.evaluation_mode = evaluation_mode
     def get_name(self):
         return "CDE" + str(self.act_rotation)
     def get_activation(self):
@@ -152,7 +220,6 @@ class subCDE_Policy:
         self.p_old = copy.deepcopy(oldp)
     def expansion(self):
         pass
-        #self.update_ewc()
     def act(self, handle, state, eps=0., eval = False):
         state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
         self.qnetwork_local.eval()
@@ -239,65 +306,7 @@ class subCDE_Policy:
 
         fisher = {n: p for n, p in fisher.items()}
         return fisher
-    def update_mas(self):
-        """
-        Updates the MAS loss by computing the importance weights (omega) and retaining previous parameter values.
-        """
-        self.qnetwork_local = self.qnetwork_local.to(self.device)
-        omega = self.compute_importance_weights(self.qnetwork_local, self.memory, self.params)
-        self.ewc_loss += self.compute_mas_loss(self.qnetwork_local, omega, self.p_old).to(self.device)
-        self.memory = ReplayBuffer(self.action_size, self.buffer_size, self.batch_size, self.device)
 
-        # Save current parameters as old parameters for future tasks
-        last_device = self.device
-        self.device = torch.device("cpu")
-        self.retain_graph = True
-        self.params = {n: p for n, p in self.qnetwork_local.named_parameters() if p.requires_grad}
-        self.p_old = {n: p.clone().detach().to(self.device) for n, p in self.params.items()}
-
-        self.qnetwork_local = self.qnetwork_local.to(last_device)
-        self.device = last_device
-
-    def compute_mas_loss(self, model, omega, p_old):
-        """
-        https://github.com/rahafaljundi/MAS-Memory-Aware-Synapses/blob/master/MAS_to_be_published/MAS.py
-        Compute the MAS loss based on importance weights (omega) and previous parameter values.
-        """
-        loss = 0
-        for n, p in model.named_parameters():
-            if "activations" in n:
-                continue
-            _loss = omega[n] * (p - p_old[n].to(self.device)) ** 2
-            loss += _loss.sum()
-        return loss
-
-    def compute_importance_weights(self, model, dataset, params):
-        """
-        Compute the importance weights (omega) for model parameters.
-        """
-        omega = {}
-        for n, p in copy.deepcopy(params).items():
-            p.data.zero_()
-            omega[n] = Variable(p.data)
-
-        model.eval()
-        states, actions, rewards, next_states, done = dataset.sample()
-
-        states = states.to(self.device)
-        for x in range(states.size(dim=0)):
-            model.zero_grad()
-            output = model(states[x], self.freeze).view(1, -1)
-            target = output.max(1)[1].view(-1)
-            negloglikelihood = F.nll_loss(F.log_softmax(output, dim=1), target)
-            negloglikelihood.backward(retain_graph=True)
-
-        for n, p in model.named_parameters():
-            if "activations" in n:
-                continue
-            omega[n].data += p.grad.data ** 2 / len(dataset.sample())
-
-        omega = {n: p for n, p in omega.items()}
-        return omega
     def _soft_update(self, local_model, target_model, tau):
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
@@ -313,19 +322,10 @@ class subCDE_Policy:
     def get_weigths(self):
         return self.qnetwork_local.get_weights()
     def extract_nn_state_dict(self):
-        """
-        Extract the state dictionary of the Q-network from the current instance.
-        """
         return self.qnetwork_local.state_dict()
     def load_nn_state_dict(self, state_dict):
-        """
-        Load the provided state dictionary into the Q-network of the current instance.
-        """
         self.qnetwork_local.load_state_dict(state_dict)
     def copy_params_from(self, other_dqn):
-        """
-        Copy parameters from another DQN model.
-        """
         self.load_state_dict(other_dqn.state_dict())
     def set_evaluation_mode(self, evaluation_mode):
         pass
@@ -388,16 +388,7 @@ class DQN_Policy:
         self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=self.learning_rate)
         self.memory = ReplayBuffer(action_size, self.buffer_size, self.batch_size, self.device)
         self.t_step = 0
-    def set_parameters(self, ql, qt, opt, ewc_loss, buffer, loss, params, oldp):
-        self.qnetwork_local.load_state_dict(ql.state_dict())
-        self.qnetwork_target.load_state_dict(qt.state_dict())
 
-        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=self.learning_rate)
-        self.ewc_loss = ewc_loss
-        self.memory = ReplayBuffer(self.action_size, self.buffer_size, self.batch_size, self.device)
-        self.loss = loss
-        self.params = params
-        self.p_old = copy.deepcopy(oldp)
     def expansion(self):
         pass
     def act(self, handle, state, eps=0., eval = False):
@@ -430,87 +421,20 @@ class DQN_Policy:
         q_expected = q_expected.to(self.device)
         q_targets = q_targets.to(self.device)
 
-        if not self.ewc_loss == 0:
-            self.ewc_loss = self.ewc_loss.to(self.device)
-
-        self.loss = F.mse_loss(q_expected, q_targets) + self.ewc_lambda * self.ewc_loss
-
+        self.loss = F.mse_loss(q_expected, q_targets)
         self.loss = self.loss.to(self.device)
 
         self.optimizer.zero_grad()
         self.loss.backward(retain_graph=True)
         self.optimizer.step()
         self._soft_update(self.qnetwork_local, self.qnetwork_target, self.tau)
-    def get_ewc_loss(self, model, fisher, p_old):
-        loss = 0
-        for n, p in model.named_parameters():
-            _loss = fisher[n] * (p - p_old[n].to(self.device)) ** 2
-            loss += _loss.sum()
-        return loss
-    def update_ewc(self):
-        self.qnetwork_local = self.qnetwork_local.to(self.device)
-        fisher_matrix = self.get_fisher_diag(self.qnetwork_local, self.memory, self.params)
-        self.ewc_loss += self.get_ewc_loss(self.qnetwork_local, fisher_matrix, self.p_old).to(self.device)
-        self.memory = ReplayBuffer(self.action_size, self.buffer_size, self.batch_size, self.device)
-
-        last_device = self.device
-        self.device = torch.device("cpu")
-        self.retain_graph = True
-        self.params = {n: p for n, p in self.qnetwork_local.named_parameters() if p.requires_grad}
-        self.p_old = {n: p.clone().detach().to(self.device) for n, p in self.params.items()}
-
-        self.qnetwork_local = self.qnetwork_local.to(last_device)
-        self.device = last_device
-    def get_fisher_diag(self, model, dataset, params):
-        fisher = {}
-        for n, p in copy.deepcopy(params).items():
-            p.data.zero_()
-            fisher[n] = Variable(p.data)
-
-        model.eval()
-        states, actions, rewards, next_states, done = dataset.sample()
-
-        states = states.to(self.device)
-        for x in range(states.size(dim=0)):
-            model.zero_grad()
-            output = model(states[x], self.freeze).view(1, -1)
-            target = output.max(1)[1].view(-1)
-            negloglikelihood = F.nll_loss(F.log_softmax(output, dim=1), target)
-            negloglikelihood.backward(retain_graph=True)
-        for n, p in model.named_parameters():
-            fisher[n].data += p.grad.data ** 2 / len(dataset.sample())
-
-        fisher = {n: p for n, p in fisher.items()}
-        return fisher
     def _soft_update(self, local_model, target_model, tau):
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
-    def save_replay_buffer(self, filename):
-        memory = self.memory.memory
-        with open(filename, 'wb') as f:
-            pickle.dump(list(memory)[-500000:], f)
-    def load_replay_buffer(self, filename):
-        with open(filename, 'rb') as f:
-            self.memory.memory = pickle.load(f)
     def get_name(self):
         return "DQN"
     def get_weigths(self):
         return self.qnetwork_local.get_weights()
-    def extract_nn_state_dict(self):
-        """
-        Extract the state dictionary of the Q-network from the current instance.
-        """
-        return self.qnetwork_local.state_dict()
-    def load_nn_state_dict(self, state_dict):
-        """
-        Load the provided state dictionary into the Q-network of the current instance.
-        """
-        self.qnetwork_local.load_state_dict(state_dict)
-    def copy_params_from(self, other_dqn):
-        """
-        Copy parameters from another DQN model.
-        """
-        self.load_state_dict(other_dqn.state_dict())
     def set_evaluation_mode(self, evaluation_mode):
         pass
     def reset_scores(self):
@@ -1480,6 +1404,14 @@ class DQN_CBP_Policy:
         self.networkEP_completions = []
         self.score_try = 0
         self.counter = 0
+        if initialweights == 0:
+            a = torch.tensor((0.02996348, 0.61690165, 2.37539147, 3.06608078, 1.52474449, 0.25281987),
+                             dtype=torch.float), \
+                torch.tensor((1.19160814, 4.40811795, 0.91111034, 0.34885983), dtype=torch.float)
+            self.weights = [a] * parameters.layer_count
+        else:
+            self.weights = copy.deepcopy(initialweights)
+
         # Device configuration
         if parameters.use_gpu and torch.cuda.is_available():
             self.device = torch.device("cuda:0")
@@ -1489,7 +1421,7 @@ class DQN_CBP_Policy:
             print("🐢 Using CPU")
 
         # Initialize Q-networks
-        self.qnetwork_local = Network(state_size, action_size).to(self.device)
+        self.qnetwork_local = DQN(state_size, action_size, self.weights, hidsize=self.hidsize).to(self.device)
         self.qnetwork_target = copy.deepcopy(self.qnetwork_local).to(self.device)
         self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=self.learning_rate)
 
@@ -1505,7 +1437,7 @@ class DQN_CBP_Policy:
         state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
         self.qnetwork_local.eval()
         with torch.no_grad():
-            action_values = self.qnetwork_local(state)
+            action_values = self.qnetwork_local(state, self.freeze)
         self.qnetwork_local.train()
 
         # Epsilon-greedy action selection
@@ -1531,8 +1463,8 @@ class DQN_CBP_Policy:
         states, actions, rewards, next_states, dones = experiences
 
         # Compute expected Q values
-        q_expected = self.qnetwork_local(states).gather(1, actions)
-        q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(-1)
+        q_expected = self.qnetwork_local(states, self.freeze).gather(1, actions)
+        q_targets_next = self.qnetwork_target(next_states, self.freeze).detach().max(1)[0].unsqueeze(-1)
         q_targets = rewards + (self.gamma * q_targets_next * (1 - dones))
         q_expected = q_expected.to(self.device)
         q_targets = q_targets.to(self.device)
@@ -1572,13 +1504,10 @@ class DQN_CBP_Policy:
                     # Determine whether to reset the neuron with the given probability
                     if torch.rand(1).item() < reset_probability:
                         print(f"Resetting neuron {i} in layer {layer}")
-
                         with torch.no_grad():
                             # Reinitialize the entire row of weights for the selected neuron
                             layer.weight[i] = nn.init.xavier_uniform_(torch.empty_like(layer.weight[i].unsqueeze(0)))
-
                             if layer.bias is not None:
-                                # Reset bias for the selected neuron
                                 layer.bias[i] = nn.init.zeros_(layer.bias[i].unsqueeze(0))
 
 
